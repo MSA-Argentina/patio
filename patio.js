c533469caaa1ac2cdd3666b226c6f9b9f479200b
Felipe Lerena <felipelerena@gmail.com>
2016-09-15 17:32:05 -0300

    adding license

--- patio.js
+++ patio.js
@@ -1,7 +1,20 @@
 /*
  *
  * Patio.js.
- * A Javascript library for handling your application playground.
+ * A Javascript library for handling your applications playground.
+ *
+ *  Patio is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Patio is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Patio.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
